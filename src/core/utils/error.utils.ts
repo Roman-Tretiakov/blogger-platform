c3b1ee4f04541512f1e99091411b6779f74216f1
Roman <thalamus.rus@gmail.com>
2026-01-03 22:04:11 +0200

    fix2 validation handler for HW

--- src/core/utils/error.utils.ts
+++ src/core/utils/error.utils.ts
@@ -1,5 +1,7 @@
 import {ValidationError} from "../types/validation-error-type";
 
-export function createErrorMessages (errors: ValidationError[]): { errorMessages: ValidationError[] } {
+export const createErrorMessages = (
+    errors: ValidationError[],
+): { errorMessages: ValidationError[] } => {
     return { errorMessages: errors };
-}
+};
