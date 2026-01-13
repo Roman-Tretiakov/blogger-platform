236915a7e744a9f145135be5ab964d6c7fadd7a5
Roman <thalamus.rus@gmail.com>
2026-01-13 23:24:15 +0200

    fix validation

--- src/core/utils/string_transforms.ts
+++ src/core/utils/string_transforms.ts
@@ -1,6 +1,6 @@
 export function isStringUrl(str: string): boolean {
   const urlPattern = new RegExp(
-    "^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$",
+    "^https?:\/\/([a-zA-Z0-9_-]+.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$",
     "i",
   );
   return urlPattern.test(str);
