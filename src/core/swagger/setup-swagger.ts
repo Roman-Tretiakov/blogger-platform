56c91f651335891082e847ba649ec02c63254c54
Roman <thalamus.rus@gmail.com>
2026-01-13 20:53:25 +0200

    add swagger docs

--- src/core/swagger/setup-swagger.ts
+++ src/core/swagger/setup-swagger.ts
@@ -1,39 +1,41 @@
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
-import {Express} from 'express';
-import {join} from "node:path";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { Express } from "express";
+import { join } from "node:path";
 
 const swaggerOptions = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Video hosting API',
-            version: '1.1.0',
-            description: 'video API'
-        },
-        tags: [
-            {name: 'Testing', description: 'API for clearing the database'},
-            {name: 'Videos', description: 'API for managing videos'}
-        ],
-        components: {
-            securitySchemes: {
-                basicAuth: {
-                    type: 'http',
-                    scheme: 'basic',
-                    description: 'Base64 encoded username and password',
-                }
-            }
-        },
-        security: [{basicAuth: []}]
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Blogger platform API",
+      version: "1.0.0",
+      description: "Open API",
     },
-    apis: [
-        join(__dirname, '../videos/docs/testing.swagger.yml'),
-        join(__dirname, '../videos/docs/videos.swagger.yml')
+    tags: [
+      { name: "Blogs", description: "API for managing blogs" },
+      { name: "Posts", description: "API for managing posts" },
+      { name: "Testing", description: "API for clearing the database"},
     ],
+    components: {
+      securitySchemes: {
+        basicAuth: {
+          type: "http",
+          scheme: "basic",
+          description: "Base64 encoded username and password",
+        },
+      },
+    },
+    security: [{ basicAuth: [] }],
+  },
+  apis: [
+    join(__dirname, "../blogs/docs/blogs.swagger.yml"),
+    join(__dirname, "../posts/docs/posts.swagger.yml"),
+    join(__dirname, "../testing/docs/testing.swagger.yml"),
+  ],
 };
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 export const setupSwagger = (app: Express) => {
-    app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 };
