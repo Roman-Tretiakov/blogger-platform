a864f563f3ac1508c783a9f09e69e9c1effd0045
Roman <thalamus.rus@gmail.com>
2026-01-28 02:25:31 +0200

    bug fix

--- src/core/utils/sort-and-pagination.utils.ts
+++ src/core/utils/sort-and-pagination.utils.ts
@@ -4,9 +4,11 @@ import { paginationAndSortingDefault } from "../middlewares/pagination-sorting-v
 export function setDefaultSortAndPaginationIfNotExist<P = string>(
   query: Partial<PaginationAndSortingType<P>>,
 ): PaginationAndSortingType<P> {
-   return  {
-    ...query,
-    ...paginationAndSortingDefault,
-    sortBy: (query.sortBy ?? paginationAndSortingDefault.sortBy) as P,
-  };
+   return {
+     ...query,
+     pageNumber: query.pageNumber ?? paginationAndSortingDefault.pageNumber,
+     pageSize: query.pageSize ?? paginationAndSortingDefault.pageSize,
+     sortDirection: query.sortDirection ?? paginationAndSortingDefault.sortDirection,
+     sortBy: (query.sortBy ?? paginationAndSortingDefault.sortBy) as P,
+   };
 }
