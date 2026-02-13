1b991945aab35886ac60143d9efbe92dddb71491
Roman <thalamus.rus@gmail.com>
2026-02-13 21:59:22 +0200

    minor fix

--- src/testing/routers/handlers/testing-handler.ts
+++ src/testing/routers/handlers/testing-handler.ts
@@ -3,10 +3,13 @@ import { HttpStatus } from "../../../core/enums/http-status";
 import { blogsRepository } from "../../../blogs/repositories/blogs.repository";
 import { postsRepository } from "../../../posts/repositories/posts.repository";
 import { usersRepository } from "../../../users/repositories/users.repository";
+import { commentsRepository } from "../../../comments/repositories/comments.repository";
 
 export const testingHandler = async (req: Request, res: Response) => {
   await postsRepository.clear();
   await blogsRepository.clear();
   await usersRepository.clear();
+  await commentsRepository.clear();
+
   res.status(HttpStatus.NoContent).send("All data is deleted");
 };
