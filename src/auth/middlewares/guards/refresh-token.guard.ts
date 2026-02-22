fe816db67bedc3b5a16337c2a2e952e5830c181e
Roman <thalamus.rus@gmail.com>
2026-02-22 23:54:56 +0200

    add refresh-token.guard

--- src/auth/middlewares/guards/refresh-token.guard.ts
+++ src/auth/middlewares/guards/refresh-token.guard.ts
@@ -1,16 +1,32 @@
 import { NextFunction, Request, Response } from "express";
 import { HttpStatus } from "../../../core/enums/http-status";
+import { jwtService } from "../../adapters/jwt.service";
+import { TokensTypes } from "../../adapters/enums/tokens-types";
 
 export const refreshTokenGuard = (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  //TODO: update refresh token validation
-  const refreshToken = req.cookies?.refreshToken?.toString();
-  if (!refreshToken) {
-    return res.status(HttpStatus.Unauthorized).send("Refresh token is missing");
-  }
+  try {
+    const refreshToken: string | undefined = req.cookies["refreshToken"];
+    if (!refreshToken) {
+      return res
+        .status(HttpStatus.Unauthorized)
+        .send("Refresh token is missing");
+    }
 
-  next();
+    const verifiedToken: any = jwtService.verifyToken(
+      refreshToken,
+      TokensTypes.RT,
+    );
+    if (!verifiedToken) {
+      return res
+        .status(HttpStatus.Unauthorized)
+        .send("Refresh token is invalid or expired");
+    }
+    next();
+  } catch (error: unknown) {
+    return res.status(HttpStatus.InternalServerError).send(`message: ${error}`);
+  }
 };
